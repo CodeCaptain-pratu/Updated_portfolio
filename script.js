c7ce0cc724a779ee9405396230a8ea2d80ef0cbf
pratiksha <pratu@gmail.com>
2024-12-16 13:46:28 +0530

    Landing page created

---
+++ script.js
@@ -0,0 +1,23 @@
+var tl=gsap.timeline();
+tl.to("#full",{
+    right:0,
+    duration:0.7,
+})
+tl.from("#full li",{
+    x:200,
+    opacity:0,
+    duration:0.2,
+    stagger:1,
+})
+tl.from("#full i",{
+    opacity:0,
+})
+tl.pause();
+let menu=document.querySelector("#nav i");
+menu.addEventListener("click",function(){
+    tl.play();
+})
+let close=document.querySelector("#full i");
+close.addEventListener("click",function(){
+    tl.reverse();
+})
